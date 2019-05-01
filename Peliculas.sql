c7de015f30dd36d813ad8f98ab749903840503af
moy01 <m.o.i.s.e.s20@hotmail.com>
2019-05-01 15:13:31 -0500

    Update Peliculas.sql
    Creacion de la tabla cliente

--- Peliculas.sql
+++ Peliculas.sql
@@ -165,3 +165,39 @@ CREATE TABLE IF NOT EXISTS `Peliculas`.`Total_pagar` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Peliculas`.`Cliente` (
+  `Id_cliente` INT NOT NULL,
+  `Nombre` VARCHAR(45) NULL,
+  `Ap` VARCHAR(45) NULL,
+  `Am` VARCHAR(45) NULL,
+  `Correo` VARCHAR(45) NULL,
+  `Telefono` VARCHAR(45) NULL,
+  `Estado_entrega` VARCHAR(45) NULL,
+  `Id_direccion` INT NOT NULL,
+  `Id_total_pagar` INT NOT NULL,
+  `Id_renta_o_venta` INT NOT NULL,
+  'Id_pelicula' INT not NULL,
+  PRIMARY KEY (`Id_cliente`),
+ CONSTRAINT `fk_Detalle_pedido_Libro1`
+    FOREIGN KEY (`Id_pelicula`)
+    REFERENCES `Peliculas`.`Pelicula` (`Id_pelicula`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION),
+FOREIGN KEY (`Id_total_pagar`)
+    REFERENCES `Peliculas`.`Total_pagar` (`Id_total_pagar`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION),
+FOREIGN KEY (`Id_renta_o_venta`)
+    REFERENCES `Peliculas`.`Renta_o_venta` (`Id_renta_o_venta`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION),
+  CONSTRAINT `fk_Usuarios_Direccion` 
+    FOREIGN KEY (`Id_direccion`)
+    REFERENCES `Peliculas`.`Direccion` (`Id_direccion`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
